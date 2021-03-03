be457ec2785359fb3d1248f50203417df1da8b56
11kymmmy30 <52526115+11kymmmy30@users.noreply.github.com>
2021-03-03 13:17:34 +0900

    3/3test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import "./styles.css";
+/**
+ * const、let等の変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var val1 = "var変数";
+console.log("val1 = " + val1);
+
+const val4 = {
+  name: "11kymmmy30",
+  age: 27
+};
+
+console.log(val4);
+val4.age = 28;
+val4.address = "Gifu";
+console.log(val4);
